64fce6e78844eeede315bf88e5d0fce6e0c6545f
Anthony Pozo <anthonypozo1@gmail.com>
2025-11-13 20:11:00 +0100

    Refractor Test For Branch

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -20,37 +20,46 @@ public class LoginController implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         if ("POST".equals(exchange.getRequestMethod())) {
-            // Leer el body de la petición
-            InputStream inputStream = exchange.getRequestBody();
-            String body = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+            boolean ok = loginVerification(exchange);
 
-            // Parsear JSON: {"username":"admin","password":"1234"}
-            JSONObject json = new JSONObject(body);
-            String username = json.getString("username");
-            String password = json.getString("password");
-
-            // Validar login
-            boolean ok = authService.login(username, password);
-
-            // Respuesta en JSON
-            JSONObject responseJson = new JSONObject();
-            responseJson.put("success", ok);
-            responseJson.put("message", ok ? "Login OK" : "Login Failed");
-
-            byte[] responseBytes = responseJson.toString().getBytes(StandardCharsets.UTF_8);
-            exchange.getResponseHeaders().add("Content-Type", "application/json");
-            exchange.sendResponseHeaders(ok ? 200 : 401, responseBytes.length);
-
-            OutputStream os = exchange.getResponseBody();
-            os.write(responseBytes);
-            os.close();
+            buildLoginResponse(exchange, ok);
         } else {
-            // Si no es POST, devolver error
-            String response = "Método no permitido";
-            exchange.sendResponseHeaders(405, response.getBytes().length);
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+            buildMethodNotAllowResponse(exchange);
         }
+    }
+
+    private static void buildMethodNotAllowResponse(HttpExchange exchange) throws IOException {
+        String response = "Method not allowed";
+        exchange.sendResponseHeaders(405, response.getBytes().length);
+        getOutputStream(exchange, response.getBytes());
+    }
+
+    private static void buildLoginResponse(HttpExchange exchange, boolean ok) throws IOException {
+        JSONObject responseJson = new JSONObject();
+        responseJson.put("success", ok);
+        responseJson.put("message", ok ? "Login OK" : "Login Failed");
+
+        byte[] responseBytes = responseJson.toString().getBytes(StandardCharsets.UTF_8);
+        exchange.getResponseHeaders().add("Content-Type", "application/json");
+        exchange.sendResponseHeaders(ok ? 200 : 401, responseBytes.length);
+
+        getOutputStream(exchange, responseBytes);
+    }
+
+    private static void getOutputStream(HttpExchange exchange, byte[] responseBytes) throws IOException {
+        OutputStream os = exchange.getResponseBody();
+        os.write(responseBytes);
+        os.close();
+    }
+
+    private boolean loginVerification(HttpExchange exchange) throws IOException {
+        InputStream inputStream = exchange.getRequestBody();
+        String body = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
+
+        JSONObject json = new JSONObject(body);
+        String username = json.getString("username");
+        String password = json.getString("password");
+
+        return authService.login(username, password);
     }
 }
