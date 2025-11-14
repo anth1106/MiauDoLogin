b561969f4102609c4289fec5e6abbde4da01492a
Anthony Pozo <anthonypozo1@gmail.com>
2025-11-14 17:57:09 +0100

    userDao creation, with csv database for users and enviroment variables

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
 import com.sun.net.httpserver.HttpServer;
 import controller.LoginController;
+import dao.UserDao;
 import service.AuthService;
 
 void main() throws Exception {
-    AuthService authService = new AuthService();
-    authService.register("admin", "1234"); // example
+    UserDao userDao = startUserDao();
+    AuthService authService = new AuthService(userDao);
+    //authService.register("admin", "1234"); // example
 
+    runServer(authService);
+}
+
+private static UserDao startUserDao() {
+    String csvPath = System.getenv("USERS_CSV_PATH");
+    if (csvPath == null) {
+        throw new RuntimeException("environment variable USERS_CSV_PATH not defined");
+    }
+    return new UserDao(csvPath);
+}
+
+private static void runServer(AuthService authService) throws IOException {
     HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
     server.createContext("/login", new LoginController(authService));
     server.start();
---
+++ src/dao/UserDao.java
@@ -0,0 +1,58 @@
+package dao;
+
+import model.User;
+import util.PasswordUtils;
+
+import java.io.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserDao {
+    private Map<String, User> users = new HashMap<>();
+    private String csvPath;
+
+    public UserDao(String csvPath) {
+        this.csvPath = csvPath;
+        loadUsersFromCsv(csvPath);
+    }
+
+    private void loadUsersFromCsv(String csvPath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(csvPath))) {
+            String line;
+            boolean firstLine = true;
+            while ((line = br.readLine()) != null) {
+                if (firstLine) { firstLine = false; continue; }
+                String[] parts = line.split(",");
+                if (parts.length == 2) {
+                    String username = parts[0].trim();
+                    String hash = parts[1].replace("\"", "").trim();
+                    users.put(username, new User(username, hash));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public User findByUsername(String username) {
+        return users.get(username);
+    }
+
+    public Collection<User> getAllUsers() {
+        return users.values();
+    }
+
+    public void saveUser(String username, String hash) {
+        User user = new User(username, hash);
+        users.put(username, user);
+
+        try (FileWriter fw = new FileWriter(csvPath, true);
+             BufferedWriter bw = new BufferedWriter(fw);
+             PrintWriter out = new PrintWriter(bw)) {
+            out.println(username + "," + hash);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
